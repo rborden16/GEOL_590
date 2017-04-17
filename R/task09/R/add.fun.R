f651c87a27d20c7aa1c2e9025cf844c2bb8165e5
Rose Borden <rose@rosemacbook.nomads.utk.edu>
2017-04-17 09:48:44 -0400

    task09 with namespace

--- R/task09/R/add.fun.R
+++ R/task09/R/add.fun.R
@@ -1,7 +1,13 @@
-#' This function adds two numbers together.
-#' @param x A number
-#' @param y A number
-#' @return The sum of \code{x} and \code{y}
+#' Add together two numbers.
+#' 
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add.fun(2, 3)
+#' add.fun(5, 5)
+#' 
+#' @export
 
 add.fun <- function(x,y) {
   x + y
